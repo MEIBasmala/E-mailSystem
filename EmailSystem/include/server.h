c919fd9f24e8db9df5c4b97d4038fa9dddff10b4
KORIAnfal <109480506+KORIAnfal@users.noreply.github.com>
2022-11-20 10:03:21 +0100

    Update server.h

--- EmailSystem/include/server.h
+++ EmailSystem/include/server.h
@@ -8,18 +8,21 @@ class Server
 
 public:
     
-    server(string []);
-    
-
-
-
-
-
-
+    server(int []);
+    //construvter for the BST
+    server();
+    //constructer for the AVL
+    server();
+    bool send(user USER,email &mail);
+    void process( email &);
+    void receive(email &mail);
+    void resend();
 
 
 private:
-
+  vector<int> data_base;  
+  std::queue<email> mainQueue;
+  std::queue<email> copyQueue;
 
 
 
