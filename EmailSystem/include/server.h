6d5d474575caf1df95177fa33282efb330bb5b07
KORIAnfal <109480506+KORIAnfal@users.noreply.github.com>
2022-11-20 10:56:59 +0100

    Update server.h

--- EmailSystem/include/server.h
+++ EmailSystem/include/server.h
@@ -1,6 +1,7 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
-
+#include "email.h"
+#include "vector"
 
 
 class Server
@@ -8,22 +9,22 @@ class Server
 
 public:
     
-    server(int []);
+    server(vector<int>);
     //construvter for the BST
     server();
     //constructer for the AVL
     server();
-    bool send(user USER,email &mail);
-    void process( email &);
+    bool send(email &mail);
+    void process();
     void receive(email &mail);
-    void resend();
+    
 
 
 private:
   vector<int> data_base;  
   std::queue<email> mainQueue;
   std::queue<email> copyQueue;
-
+  bool resend();
 
 
 
