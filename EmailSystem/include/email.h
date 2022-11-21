fca45e2f1aa7d8c28f0ec0c652c5172d51cd24a8
KORIAnfal <109480506+KORIAnfal@users.noreply.github.com>
2022-11-21 02:04:42 +0100

    Update email.h

--- EmailSystem/include/email.h
+++ EmailSystem/include/email.h
@@ -12,31 +12,6 @@ struct user
     int id;
     std::string name;
     std::stack<email> mailBox;
-
-    void print_mail_box()
-    {
-        std::cout << "\nDisplaying Your MailBox . . . \n";
-        if (mailBox.size() == 0)
-        {
-            std::cout << "\n--------------------------------------------\n";
-            std::cout << "\nYour MailBox is empty !!\n";
-            std::cout << "\n--------------------------------------------\n";
-        }
-        else
-        {
-            std::cout << "\n***************************************************\n";
-            for (int i = 0; i < mailBox.size(); i++)
-            {
-                std::cout << "\n--------------------------------------------\n";
-                //mailBox.top().print();
-                mailBox.pop();
-                std::cout << "\n--------------------------------------------\n";
-            }
-        }
-    }
-
-
-
 };
 
 class email
@@ -51,15 +26,20 @@ public:
     email();
     email(const user &s, const user &r, const std::string &msg);
     email(const user &s, const user &r, std::string &&msg);
+
     void set_sender(const user &newsender);
     void set_receiver(const user &newreceiver);
+
     void set_text(std::string);
     std::string get_text();
+
     user get_sender() const;
     user get_receiver() const;
+
     std::string get_sender_name() const;
     std::string get_receiver_name() const;
-    void print();
+
+
     ~email(){};
 };
 
