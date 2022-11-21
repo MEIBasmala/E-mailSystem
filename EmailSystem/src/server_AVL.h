a479364a17aa6fd9d49e09952d6f3177c00e0b40
KORIAnfal <109480506+KORIAnfal@users.noreply.github.com>
2022-11-21 18:00:48 +0100

    Create server_AVL.h

---
+++ EmailSystem/src/server_AVL.h
@@ -0,0 +1,29 @@
+#ifndef __SERVER_AVL_H__
+#define __SERVER_AVL_H__
+#include "email.h"
+#include"AVLTree.h"
+#include<queue>
+class Server_AVL
+{
+
+public:
+    Server_AVL();
+    bool send_to_server(const email &mail);
+    void process();
+    bool sign_up(const user &newuser);
+
+
+
+private:
+    //using a vector for part A
+    AVL<int> DataBase_AVL;
+    std::queue<email> mainQueue;
+    std::queue<email> copyQueue;
+    bool resend(email &mail);
+    bool send(email &mail);
+    bool receive(email &mail);
+    bool check(user USER);
+    void CreateDataBase();
+};
+
+#endif
