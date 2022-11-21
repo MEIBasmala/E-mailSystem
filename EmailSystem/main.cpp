f6904ae0bc8980d9038364d0c5628b98eba4046d
Samah Farez <97588511+SamahFarez@users.noreply.github.com>
2022-11-21 10:14:13 +0100

    Update main.cpp

--- EmailSystem/main.cpp
+++ EmailSystem/main.cpp
@@ -5,8 +5,6 @@
 #include "server.h"
 
 
-using std::vector;
-
 int IDchecker(int INPUT)
 {
 
@@ -14,7 +12,7 @@ int IDchecker(int INPUT)
     {
         if (INPUT < 110000 || INPUT > 119999)
         {
-            std::cout << "Invalid ID Retry again\nEnter ID:";
+            std::cout << "Invalid ID !!!\nRetry again!!\nEnter ID:";
             std::cin >> INPUT;
         }
         else
@@ -34,6 +32,7 @@ int main()
 
     bool BOOL = true;
     int input;
+
     while (BOOL)
     {
 
@@ -41,17 +40,18 @@ int main()
         std::cout << "What do you want to do ?\n";
         std::cout << " [ 1 ] -> SEND AN EMAIL\n";
         std::cout << " [ 2 ] -> SIGN UP\n";
-        std::cout << " [ 3 ] -> SHUT DOWN SERVER\n";
+        std::cout << " [ 3 ] -> CHECK MAILBOX\n";
+        std::cout << " [ 4 ] -> SHUT DOWN SERVER\n";
         std::cout << "Remark : All IDs are of the form 11****\n ";
         std::cout << "--------------------------------------------------------------------------\n";
 
-        std::cout << "==>";
+        std::cout << "==> ";
         std::cin >> input;
 
         if (input == 1)
         {
-
-            std::cout << "Welcome to our services center,Please enter the sender and reciever info\n " << std::endl;
+            clear();
+            std::cout << "Welcome to our services center !! \nPlease enter the sender and reciever info !" << std::endl;
 
             user sender;
             user receiver;
@@ -72,15 +72,15 @@ int main()
             getline(std::cin >> std::ws, txt);
 
             email new_mail(sender, receiver, txt);
-
-            EmailServer.send_to_server(new_mail);
-
-            clear();
+            if (EmailServer.send_to_server(new_mail) == false)
+            {
+                input = 2;
+            }
         }
 
         if (input == 2)
         {
-
+            clear();
             std::cout << "WELCOME TO THE USER CENTER !\nTo Sign up, enter your name and ID number\nPlease Make Sure The ID is of the Format 11**** " << std::endl;
 
             user NewUser;
@@ -91,16 +91,27 @@ int main()
             std::cin >> NewUser.id;
             NewUser.id = IDchecker(NewUser.id);
 
-            EmailServer.sign_up(NewUser);
-
-        clear();
+            if (EmailServer.sign_up(NewUser))
+                std::cout << "Signed Up Successfully !! \nYou'll be redirected to our services center !!!" << std::endl;
         }
 
         if (input == 3)
+        {
+            user ME;
+            std::cout << "Your ID :";
+            std::cin >> ME.id;
+            ME.id = IDchecker(ME.id);
+
+            EmailServer.process();
+            ME.print_mail_box();
+        }
+
+        if (input == 4)
             BOOL = false;
     }
 
     std::cout << "THANK YOU FOR USING OUR SERVICES, SEE YA!! ";
 
-
+    return 0;
 }
+
